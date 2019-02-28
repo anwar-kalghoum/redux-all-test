a442c04ed47de5d06be29b0cf2bb07a197434e9d
anwar-kalghoum <anwarkalghoum@oyez.fr>
2019-02-28 09:17:53 +0100

    fix add

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
 import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+ 
+describe('<App />', () => {
+  it('renders three <App /> components', () => {
+    const wrapper = shallow(<App />);
+    // console.log(wrapper.debug());// console log() DOM
+    expect(wrapper).toMatchSnapshot();// Snapshot Test
+    console.log('ddee', expect(wrapper).toBeTruthy());
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.find('.as').text()).toEqual('aaaa');
+    expect(wrapper.find('.as').text()).toEqual('aaaa');
+    expect(wrapper.find('.as').text()).toBe('aaaa');
+    expect(wrapper.find('.as')).toBeTruthy();
+    expect(wrapper.find('.as').text()).toHaveLength(4);
+    expect(wrapper.find('.as').not('.a').text()).toHaveLength(4);
+    expect(wrapper.find('p').children().text()).toHaveLength(4);
+    expect(wrapper.find('.in-bar').text()).toHaveLength(5);
+    expect(wrapper.find('.in-bar').text()).toHaveLength(5);
+  });
 });
